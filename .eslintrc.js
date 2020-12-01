75a9d1f17f90ffb16d1d480148fc908bf7b83d90
Piotr Sornek <kebabozaur@gmail.com>
2020-12-01 20:52:10 +0100

    Run prettier on all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2020: true,
     node: true,
-    jest: true
+    jest: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'prettier/react'],
   parserOptions: {

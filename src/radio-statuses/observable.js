bab5a5e065c69019e354d2a42ba0d1a1781dfcbe
Piotr Sornek <dymanooo@protonmail.com>
2020-12-06 15:28:36 +0100

    Create radioStatusesObservable definition and tests
    (cherry picked from commit d08efd780384224dd32dd05d10320e1eeee77c2b)

---
+++ src/radio-statuses/observable.js
@@ -0,0 +1,10 @@
+// used in jsdoc
+// eslint-disable-next-line no-unused-vars
+import { Observable } from 'rxjs';
+
+import { refreshRateInSeconds } from './refresh-rate';
+
+/**
+ * @type {Observable<RadioStatus[]>}
+ */
+export const radioStatusesObservable = undefined; // todo

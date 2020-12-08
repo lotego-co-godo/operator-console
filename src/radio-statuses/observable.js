db2e784036d9d643f54f19d9b9f3291781813ab5
Piotr Sornek <dymanooo@protonmail.com>
2020-12-08 17:45:35 +0100

    Add missing import in observable.js

--- src/radio-statuses/observable.js
+++ src/radio-statuses/observable.js
@@ -1,6 +1,6 @@
 // used in jsdoc
 // eslint-disable-next-line no-unused-vars
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 import { fetchRadioStatuses } from './fetch';
 import { refreshRateInSeconds } from './refresh-rate';
 

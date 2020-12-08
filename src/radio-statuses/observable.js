0500cf243b33121c49531a7f9a74669d592459c4
Marco361 <marek.giel@wp.pl>
2020-12-08 10:22:05 +0100

    Stworzenie Observable
    (cherry picked from commit 1c3aec8277ad44ec240360ad0f1a0ec87e90ac60)

--- src/radio-statuses/observable.js
+++ src/radio-statuses/observable.js
@@ -1,10 +1,23 @@
 // used in jsdoc
 // eslint-disable-next-line no-unused-vars
 import { Observable } from 'rxjs';
-
+import { fetchRadioStatuses } from './fetch';
 import { refreshRateInSeconds } from './refresh-rate';
 
 /**
  * @type {Observable<RadioStatus[]>}
  */
-export const radioStatusesObservable = undefined; // todo
+
+const interval = refreshRateInSeconds * 1000;
+export const radioStatusesObservable = new Observable(function subscribe(subscriber) {
+  const radioStatusesPromise = fetchRadioStatuses();
+  radioStatusesPromise.then((radioStatuses) => subscriber.next(radioStatuses));
+  const intervalId = setInterval(() => {
+    const radioStatusesPromise = fetchRadioStatuses();
+    radioStatusesPromise.then((radioStatuses) => subscriber.next(radioStatuses));
+  }, interval);
+
+  return function unsubscribe() {
+    clearInterval(intervalId);
+  };
+});

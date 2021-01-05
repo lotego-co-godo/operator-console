d8d6f2fe78bfaa99217b617d9d6d5d9998145d5b
Piotr Sornek <dymanooo@protonmail.com>
2021-01-05 17:19:31 +0100

    Setup react-leaflet

--- src/components/DevicesMap/DevicesMap.js
+++ src/components/DevicesMap/DevicesMap.js
@@ -2,26 +2,26 @@ import React from 'react';
 import './DevicesMap.css';
 import PropTypes from 'prop-types';
 import { RadioStatus } from '../../radio-statuses/types';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 export function DevicesMap(props) {
   const { radioStatuses } = props;
   const firstDevice = radioStatuses[0];
+  const firstDevicePosition = [firstDevice.Position.Lat, firstDevice.Position.Lon];
 
   return (
     <div className='DevicesMap'>
-      <div>
-        Urządzenie:
-        <br />
-        Id: {firstDevice.Id}
-        <br />
-        Name: {firstDevice.Name}
-        <br />
-        Type: {firstDevice.Type}
-        <br />
-        Strength: {firstDevice.Strength}
-        <br />
-        BatteryLevel: {firstDevice.BatteryLevel}
-      </div>
+      <MapContainer center={firstDevicePosition} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={firstDevicePosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable. <br /> {firstDevice.BatteryLevel}
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }

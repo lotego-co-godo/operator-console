8797729bf3b65cf488207e43b195537ed41fbbd4
Piotr Sornek <dymanooo@protonmail.com>
2021-01-06 16:47:55 +0100

    Make SignalStrength change icon basing on strength

--- src/components/DevicesTable/pictograms/SignalStrength/SignalStrength.js
+++ src/components/DevicesTable/pictograms/SignalStrength/SignalStrength.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './SignalStrength.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignal } from '@fortawesome/free-solid-svg-icons';
+import signalFull from './icons/signal-full.svg';
+import signal4Bars from './icons/signal-4-bars.svg';
+import signal3Bars from './icons/signal-3-bars.svg';
+import signal2Bars from './icons/signal-2-bars.svg';
+import signal1Bar from './icons/signal-1-bar.svg';
+import signalEmpty from './icons/signal-empty.svg';
 
-// todo
-// eslint-disable-next-line
 export function SignalStrength({ strength }) {
   return (
     <div className='SignalStrength pictogram'>
-      <FontAwesomeIcon icon={faSignal} size='2x' />
+      <img
+        alt='signal-strength'
+        src={
+          strength === 100
+            ? signalFull
+            : strength > 60
+            ? signal4Bars
+            : strength > 40
+            ? signal3Bars
+            : strength > 20
+            ? signal2Bars
+            : strength > 0
+            ? signal1Bar
+            : signalEmpty
+        }
+      />
     </div>
   );
 }

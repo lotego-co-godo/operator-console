3ca4e3e66100cdc22c525e2e773357dced388fa2
Piotr Sornek <dymanooo@protonmail.com>
2021-01-06 18:18:57 +0100

    Integrate DevicesMap into App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import { DevicesTable } from '../DevicesTable';
 import { useObservable } from '../../hooks';
 import { radioStatusesObservable } from '../../radio-statuses';
+import { DevicesMap } from '../DevicesMap';
 
 function App() {
   const radioStatuses = useObservable(radioStatusesObservable);
 
   return (
-    <div className='App'>{radioStatuses ? <DevicesTable radioStatuses={radioStatuses} /> : <div>Loading...</div>}</div>
+    <div className='App'>
+      {radioStatuses ? (
+        <>
+          <DevicesMap radioStatuses={radioStatuses} />
+          <DevicesTable radioStatuses={radioStatuses} />
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
   );
 }
 

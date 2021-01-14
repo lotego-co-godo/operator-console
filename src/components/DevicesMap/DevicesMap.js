1f0f1e294aab70bc6c2df9c5b5ffc0a6a90cfd0c
Piotr Sornek <dymanooo@protonmail.com>
2021-01-14 21:03:49 +0100

    Fix syntax errors

--- src/components/DevicesMap/DevicesMap.js
+++ src/components/DevicesMap/DevicesMap.js
@@ -9,7 +9,7 @@ import carDarkOrangeIcon from './icons/car-solid-dark-orange.svg';
 import carLightGreenIcon from './icons/car-solid-light-green.svg';
 import carLightOrangeIcon from './icons/car-solid-light-orange.svg';
 import carRedIcon from './icons/car-solid-red.svg';
-import carYellowIcon from './icons/car-solid-yellow.svg'; 
+import carYellowIcon from './icons/car-solid-yellow.svg';
 import homeDarkGreenIcon from './icons/home-solid-dark-green.svg';
 import homeDarkOrangeIcon from './icons/home-solid-dark-orange.svg';
 import homeLightGreenIcon from './icons/home-solid-light-green.svg';
@@ -23,7 +23,6 @@ import phoneLightOrangeIcon from './icons/mobile-alt-solid-light-orange.svg';
 import phoneRedIcon from './icons/mobile-alt-solid-red.svg';
 import phoneYellowIcon from './icons/mobile-alt-solid-red.svg';
 
-
 export function DevicesMap(props) {
   const { radioStatuses } = props;
 
@@ -35,7 +34,11 @@ export function DevicesMap(props) {
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
         {radioStatuses.map((device, i) => (
-          <Marker position={[device.Position.Lat, device.Position.Lon]} key={i} icon={getIcon(device.Type)}>
+          <Marker
+            position={[device.Position.Lat, device.Position.Lon]}
+            key={i}
+            icon={getIcon(device.Type, deviceHealth(device))}
+          >
             <Popup>
               Id: {device.Id} <br />
               Strength: {device.Strength} <br />
@@ -48,57 +51,59 @@ export function DevicesMap(props) {
   );
 }
 
-function DevicesHealth(device){
-  let deviceHealth = ((device.Strength*10 + device.BatteryLevel)/200);
+function deviceHealth(device) {
+  return (device.Strength * 10 + device.BatteryLevel) / 2;
 }
 
-function getIcon(deviceType, deviceHealth){
-  let deviceColor;
+function getIcon(deviceType, deviceHealth) {
+  let deviceIcon;
+
+  if (deviceType === 'Portable') {
+    if (deviceHealth >= 90) {
+      deviceIcon = phoneDarkGreenIcon;
+    } else if (deviceHealth <= 89 && deviceHealth >= 70) {
+      deviceIcon = phoneLightGreenIcon;
+    } else if (deviceHealth <= 69 && deviceHealth >= 50) {
+      deviceIcon = phoneYellowIcon;
+    } else if (deviceHealth <= 49 && deviceHealth >= 30) {
+      deviceIcon = phoneLightOrangeIcon;
+    } else if (deviceHealth <= 29 && deviceHealth >= 10) {
+      deviceIcon = phoneDarkOrangeIcon;
+    } else {
+      deviceIcon = phoneRedIcon;
+    }
+  } else if (deviceType === 'Car') {
+    if (deviceHealth >= 90) {
+      deviceIcon = carDarkGreenIcon;
+    } else if (deviceHealth <= 89 && deviceHealth >= 70) {
+      deviceIcon = carLightGreenIcon;
+    } else if (deviceHealth <= 69 && deviceHealth >= 50) {
+      deviceIcon = carYellowIcon;
+    } else if (deviceHealth <= 49 && deviceHealth >= 30) {
+      deviceIcon = carLightOrangeIcon;
+    } else if (deviceHealth <= 29 && deviceHealth >= 10) {
+      deviceIcon = carDarkOrangeIcon;
+    } else {
+      deviceIcon = carRedIcon;
+    }
+  } else {
+    if (deviceHealth >= 90) {
+      deviceIcon = homeDarkGreenIcon;
+    } else if (deviceHealth <= 89 && deviceHealth >= 70) {
+      deviceIcon = homeLightGreenIcon;
+    } else if (deviceHealth <= 69 && deviceHealth >= 50) {
+      deviceIcon = homeYellowIcon;
+    } else if (deviceHealth <= 49 && deviceHealth >= 30) {
+      deviceIcon = homeLightOrangeIcon;
+    } else if (deviceHealth <= 29 && deviceHealth >= 10) {
+      deviceIcon = homeDarkOrangeIcon;
+    } else {
+      deviceIcon = homeRedIcon;
+    }
+  }
+
   return new L.Icon({
-    iconUrl: 
-    if(deviceType === 'Portable'){
-      if(deviceHealth>=90){
-        deviceColor = phoneDarkGreenIcon;
-      }else if((deviceHealth<=89)&&(deviceHealth>=70)){
-        deviceColor = phoneLightGreenIcon;
-      }else if((deviceHealth<=69)&&(deviceHealth>=50)){
-        deviceColor = phoneYellowIcon;
-      }else if((deviceHealth<=49)&&(deviceHealth>=30)){
-        deviceColor = phoneLightOrangeIcon;
-      }else if((deviceHealth<=29)&&(deviceHealth>=10)){
-        deviceColor = phoneDarkOrangeIcon;
-      }else{
-        deviceColor = phoneRedIcon;
-      }
-    }else if(deviceType === 'Car'){
-      if(deviceHealth>=90){
-        deviceColor = carDarkGreenIcon;
-      }else if((deviceHealth<=89)&&(deviceHealth>=70)){
-        deviceColor = carLightGreenIcon;
-      }else if((deviceHealth<=69)&&(deviceHealth>=50)){
-        deviceColor = carYellowIcon;
-      }else if((deviceHealth<=49)&&(deviceHealth>=30)){
-        deviceColor = carLightOrangeIcon;
-      }else if((deviceHealth<=29)&&(deviceHealth>=10)){
-        deviceColor = carDarkOrangeIcon;
-      }else{
-        deviceColor = carRedIcon;
-      }
-    }else{
-      if(deviceHealth>=90){
-        deviceColor = homeDarkGreenIcon;
-      }else if((deviceHealth<=89)&&(deviceHealth>=70)){
-        deviceColor = homeLightGreenIcon;
-      }else if((deviceHealth<=69)&&(deviceHealth>=50)){
-        deviceColor = homeYellowIcon;
-      }else if((deviceHealth<=49)&&(deviceHealth>=30)){
-        deviceColor = homeLightOrangeIcon;
-      }else if((deviceHealth<=29)&&(deviceHealth>=10)){
-        deviceColor = homeDarkOrangeIcon;
-      }else{
-        deviceColor = homeRedIcon;
-      }
-    },
+    iconUrl: deviceIcon,
     iconSize: [35, 40],
   });
 }
